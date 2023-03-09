75ae147ab163d530738563f1acbd7f0cf9063bb6
Katja Loeffler <katja.loeffler88@gmail.com>
2023-03-09 14:20:24 +0100

    index musicians

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :packages, only: [:new, :create]
+  resources :musicians do
+    resources :packages, only: [:new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
