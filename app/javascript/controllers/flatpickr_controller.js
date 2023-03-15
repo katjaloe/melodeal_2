6ab0403d80e811914e0e4d6e765199e72720473a
Katja Loeffler <katja.loeffler88@gmail.com>
2023-03-15 12:38:14 +0100

    styled datepicker

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
+  // Inform the controller that it has two targets in the form, which are our inputs
+  static targets = [ "startDate" ]
+
   connect() {
+    flatpickr(this.startDateTarget, { enableTime: true, defaultHour: 18, defaultMinute: 0, inline: true})
+    // flatpickr(this.startTimeTarget, {})
+
   }
 }
