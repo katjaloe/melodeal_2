e8f74be826d61387c169044ca1599a94bda0af88
Louise Wedel <louise.wedel@enportal.de>
2023-03-15 14:32:06 +0100

    packages have a title now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_092517) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_133019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_092517) do
     t.bigint "musician_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["musician_id"], name: "index_packages_on_musician_id"
   end
 
